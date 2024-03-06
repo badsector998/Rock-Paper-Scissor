9b201fee2bf6c784578592eaf4c0a23fd5a85ad1
badsector998 <aprilito.ik@outlook.com>
2024-03-06 22:08:34 +0700

    Initial commit

---
+++ lib/rock_paper_scissor.ex
@@ -0,0 +1,43 @@
+defmodule RockPaperScissor do
+
+  use Application
+
+  def start(_type, _args) do
+    RockPaperScissor.main()
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
+
+  def main() do
+    rps = [:Rock, :Paper, :Scissor]
+    computer = Enum.random(rps)
+    player = IO.gets("Bet on!\n") |> String.trim() |> String.to_existing_atom()
+    check(computer, player)
+  end
+
+  def check(computer, player) do
+
+    victories = %{
+      Rock: :Paper,
+      Paper: :Scissor,
+      Scissor: :Rock,
+    }
+
+    cond do
+      computer === player ->
+        IO.puts("It's a tie!")
+      computer === victories[player] ->
+        IO.puts("Computer Wins!")
+      true ->
+        IO.puts("You won!")
+    end
+
+    again = IO.gets("Wanna Play Again?\n") |> String.trim()
+    if again === "y" do
+      main()
+    else
+      IO.puts("Thank You For Playing!")
+      exit(:normal)
+    end
+  end
+
+end
